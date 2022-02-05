69effc903658b62352306ca92be95a39f6570ccf
Uvaish <uvaishkhan9219@gmail.com>
2022-02-05 19:06:33 +0530

    changes

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,3 @@ Route::get('/home', function () {
     return "Home";
 });
 Route::resource('/events/', App\Http\Controllers\EventManagementController::class);
-// Route::get('/learner/post/title/{id}', [App\Http\Controllers\PostController::class, 'postTitle'])->name('learner.postTitle');
-// Route::post('/learner/post/update-title', [App\Http\Controllers\PostController::class, 'updatePostTitle'])->name('learner.updatePostTitle');
